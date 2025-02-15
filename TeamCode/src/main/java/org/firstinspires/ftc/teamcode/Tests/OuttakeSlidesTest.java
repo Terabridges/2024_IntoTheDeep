d2b7fa737af4a753a1a6ced8177727695a1c7aff
JoshBeitler47 <beitlerjosh@gmail.com>
2025-02-15 18:38:28 -0500

    Linear Test & Config Names

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/OuttakeSlidesTest.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp(name="TestOpMode", group="Test")
+@Config
+public class OuttakeSlidesTest extends LinearOpMode {
+    public DcMotor outtakeTopVertical;
+    public DcMotor outtakeBottomVertical;
+
+    public Gamepad currentGamepad1 = new Gamepad();
+    public Gamepad previousGamepad1 = new Gamepad();
+
+    public enum Mode {
+        BOTH,
+        INDIVIDUAL
+    }
+
+    @Override
+    public void runOpMode() {
+        outtakeTopVertical = hardwareMap.get(DcMotor.class, "outtake_bottom_vertical");
+        outtakeBottomVertical = hardwareMap.get(DcMotor.class, "outtake_top_vertical");
+        outtakeBottomVertical.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        Mode mode = Mode.BOTH;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            previousGamepad1.copy(currentGamepad1);
+            currentGamepad1.copy(gamepad1);
+
+            switch (mode) {
+                case BOTH:
+                    outtakeBottomVertical.setPower(gamepad1.left_stick_y);
+                    outtakeTopVertical.setPower(gamepad1.left_stick_y);
+                    break;
+                case INDIVIDUAL:
+                    outtakeBottomVertical.setPower(gamepad1.left_stick_y);
+                    outtakeTopVertical.setPower(gamepad1.right_stick_y);
+            }
+
+            if (currentGamepad1.a && !previousGamepad1.a) {
+                switch (mode) {
+                    case BOTH:
+                        mode = Mode.INDIVIDUAL;
+                        break;
+                    case INDIVIDUAL:
+                        mode = Mode.BOTH;
+                        break;
+                }
+            }
+
+            telemetry.addData("Currently", mode);
+            telemetry.update();
+        }
+    }
+}
