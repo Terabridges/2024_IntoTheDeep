00d039c27b8109c255d265faca9dc0aaf197d1be
sidnambiar2008 <sidnambiar2008@gmail.com>
2025-02-27 22:05:02 -0500

    Got really accurate distance measures.
    I can simply get the angle accurately If I need to with arctan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/SpecimenAprilDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/SpecimenAprilDetection.java
@@ -145,7 +145,7 @@ public class SpecimenAprilDetection extends LinearOpMode {
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
                 telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x * (1.5504), detection.ftcPose.y * (1.8582), detection.ftcPose.z * (1.2352)));
                 telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                 telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
             } else {
