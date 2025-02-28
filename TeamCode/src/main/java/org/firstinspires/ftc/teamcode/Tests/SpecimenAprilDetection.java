5de855aa68e2f26b741245c79e205e19ebe5e237
sidnambiar2008 <sidnambiar2008@gmail.com>
2025-02-27 19:46:14 -0500

    Some Minor Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/SpecimenAprilDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/SpecimenAprilDetection.java
@@ -60,8 +60,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Concept: AprilTag Easy", group = "Concept")
-@Disabled
+@TeleOp(name = "AprilTagDetection")
 public class SpecimenAprilDetection extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
