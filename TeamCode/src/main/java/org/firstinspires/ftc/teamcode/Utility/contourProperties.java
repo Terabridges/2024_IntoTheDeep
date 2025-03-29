c7068cfebbd00bc3728b6564d2f8d36b69f2a6c3
JoshBeitler47 <beitlerjosh@gmail.com>
2025-03-29 19:25:07 -0400

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/contourProperties.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Utility;
+
+public class contourProperties {
+    private BlockColor color;
+    private double distance;
+    private double angle;
+    private int area;
+
+    enum BlockColor
+    {
+        RED,
+        YELLOW,
+        BLUE
+    }
+
+    public contourProperties(BlockColor color, double distance,  double angle, int area)
+    {
+        this.color = color;
+        this.distance = distance;
+        this.angle = angle;
+        this.area = area;
+    }
+
+    public BlockColor getColor()
+    {
+        return color;
+    }
+
+    public double getDistance()
+    {
+        return distance;
+    }
+
+    public double getAngle()
+    {
+        return angle;
+    }
+
+    public int getArea()
+    {
+        return area;
+    }
+}
